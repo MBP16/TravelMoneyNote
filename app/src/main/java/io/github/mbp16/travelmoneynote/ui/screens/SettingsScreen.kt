50d47a8eb90cac6c1271322e91d4d098608bd2cd
Hyunwoo Park <phw1508@naver.com>
2026-01-23 17:38:58 +0800

    Fix Bug

--- app/src/main/java/io/github/mbp16/travelmoneynote/ui/screens/SettingsScreen.kt
+++ app/src/main/java/io/github/mbp16/travelmoneynote/ui/screens/SettingsScreen.kt
@@ -13,6 +13,7 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material.icons.automirrored.filled.ArrowForward
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Check
 import androidx.compose.material.icons.filled.Delete
