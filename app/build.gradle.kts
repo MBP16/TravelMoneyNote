955e26cec54c7913033ffb2b16f2a8a9f2b8a535
Hyunwoo Park <phw1508@naver.com>
2026-01-23 18:12:05 +0800

    bump version code to 12

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
         applicationId = "io.github.mbp16.travelmoneynote"
         minSdk = 34
         targetSdk = 36
-        versionCode = 11
+        versionCode = 12
         versionName = "1.4.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
